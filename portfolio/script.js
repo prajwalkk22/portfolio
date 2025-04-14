9b4eb35ebb356cbe04298bf9cb2b0e15a286ca71
prajwalkk22 <prajwal11kk22@gmail.com>
2025-04-14 21:59:43 +0530

    feat: Refined About section layout and enhanced profile image styling

---
+++ portfolio/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Backend Developer", "Java Developer", "DevOps Enthusiasist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
+  
